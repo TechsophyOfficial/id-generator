d5282e4024c9eab433195e7e2d3e3ae749cde804
Ayush Garg <imaygarg@gmail.com>
2022-08-24 16:27:34 +0530

    Code Coverage

--- src/test/java/com/techsophy/idgenerator/listeners/CustomMongoEventListenerTest.java
+++ src/test/java/com/techsophy/idgenerator/listeners/CustomMongoEventListenerTest.java
@@ -28,33 +28,54 @@ class CustomMongoEventListenerTest {
     @InjectMocks
     CustomMongoEventListener customMongoEventListener;
 
-    @Mock
-    BeforeConvertEvent<Object> beforeConvertEvent;
+//    @Mock
+//    BeforeConvertEvent<Object> beforeConvertEvent;
 
-    @Mock
-    MongoService mongoService;
-
+//    @Mock
+//    MongoService mongoService;
+//
     @Mock
     ObjectMapper objectMapper;
 
     @Test
     void onBeforeConvertTest() {
 
-        CustomMongoEventListener obj = new CustomMongoEventListener(mongoService, objectMapper);
-        Map<String, Object> source = Map.ofEntries(
-                new AbstractMap.SimpleEntry<String, Object>(FORM_ID, SALES_ORDER_FORM_ID),
-                new AbstractMap.SimpleEntry<String, Object>(FORM_DATA, SALES_ORDER_ID));
-        BeforeConvertEvent<Object> eventObject = new BeforeConvertEvent<>(source, "abc");
+//        CustomMongoEventListener obj = new CustomMongoEventListener(mongoService, objectMapper);
+//        Map<String, Object> source = Map.ofEntries(
+//                new AbstractMap.SimpleEntry<String, Object>(FORM_ID, SALES_ORDER_FORM_ID),
+//                new AbstractMap.SimpleEntry<String, Object>(FORM_DATA, SALES_ORDER_ID));
+//        BeforeConvertEvent<Object> eventObject = new BeforeConvertEvent<>(source, "abc");
+//
+//        Map<String, Object> formData = Map.ofEntries(
+//                new AbstractMap.SimpleEntry<String, Object>(SALES_ORDER_ID, SALES_ORDER_FORM_ID));
+//                //new AbstractMap.SimpleEntry<String, Object>(SALES_QUOTE_ID, SALES_ORDER_FORM_ID));
+//
+//        Mockito.when(objectMapper.convertValue(any(), eq(new TypeReference<>() {
+//        }))).thenReturn(formData);
+//        //EventObject event = new EventObject(source);
+//
+//        //Assertions.assertNotNull(obj.onBeforeConvert(eventObject));
+//        obj.onBeforeConvert(eventObject);
 
         Map<String, Object> formData = Map.ofEntries(
-                new AbstractMap.SimpleEntry<String, Object>(SALES_ORDER_ID, SALES_ORDER_FORM_ID));
+                new AbstractMap.SimpleEntry<String, Object>("abc", "abc"));
+
+        Map<String, Object> source = Map.ofEntries(
+                new AbstractMap.SimpleEntry<String, Object>(FORM_ID, SALES_ORDER_FORM_ID),
+                new AbstractMap.SimpleEntry<String, Object>(FORM_DATA, formData));
+        BeforeConvertEvent<Object> eventObject = new BeforeConvertEvent<>(source, "abc");
+
+
                 //new AbstractMap.SimpleEntry<String, Object>(SALES_QUOTE_ID, SALES_ORDER_FORM_ID));
 
-        Mockito.when(objectMapper.convertValue(any(), eq(new TypeReference<>() {
-        }))).thenReturn(formData);
-        //EventObject event = new EventObject(source);
+        Mockito.when(objectMapper.convertValue(any(), (Class<Object>) any())).thenReturn(formData);
 
-        //Assertions.assertNotNull(obj.onBeforeConvert(eventObject));
-        obj.onBeforeConvert(eventObject);
+
+
+        customMongoEventListener.onBeforeConvert(eventObject);
+
+
+
+
     }
 }
